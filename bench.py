04d885bfb5b5b2e8cb35aa618119bcf0785e4e16
Michael Eden <michael.eden100@gmail.com>
2022-07-28 14:02:11 -0400

    Implement and bench merge_tracks, refactor and update bench.py

--- bench.py
+++ bench.py
@@ -1,25 +1,111 @@
-import os
+import asyncio
+import cProfile
+import io
+import time
 import timeit
+from pstats import SortKey
+from typing import Callable, List
 
 import mido
 import numpy as np
 
+import utils
 
-def bench_midi_loading() -> float:
-    """Compute average time to load MIDI file."""
+
+def load_midi_files(number: int) -> List[mido.MidiFile]:
     rng = np.random.default_rng(seed=1845)
-    filepaths = [
-        os.path.join(root, filename)
-        for root, _, filenames in os.walk('res')
-        for filename in filenames
-    ]
+    filepaths = utils.get_midi_filepaths()
+
+    midi_files = []
+    while len(midi_files) < number:
+        filepath = filepaths[rng.integers(0, len(filepaths))]
+        try:
+            mid = mido.MidiFile(filepath)
+            midi_files.append(mid)
+        except Exception as e:
+            print(f'Error reading MIDI file: {e}')
+
+    return midi_files
+
+
+def load_midi_file_fn() -> Callable:
+    """Return function which loads a random MIDI file."""
+    rng = np.random.default_rng(seed=1845)
+    filepaths = utils.get_midi_filepaths()
 
     def load_midi_file():
-        filepath = filepaths[rng.integers(0, len(filepaths))]
-        mido.MidiFile(filepath)
+        try:
+            mido.MidiFile(filepaths[rng.integers(0, len(filepaths))])
+        except Exception as e:
+            print(f'Error reading MIDI file: {e}')
 
-    return timeit.timeit(load_midi_file, number=10)
+    return load_midi_file
 
 
-if __name__ == '__main__':
-    print(bench_midi_loading())
+def load_midi_file_fn_async():
+    rng = np.random.default_rng(seed=1845)
+    filepaths = utils.get_midi_filepaths()
+
+    async def load_midi_file():
+        try:
+            mido.MidiFile(filepaths[rng.integers(0, len(filepaths))])
+        except Exception as e:
+            print(f'Error reading MIDI file: {e}')
+
+    return load_midi_file
+
+
+def merge_tracks_fn(number: int) -> Callable:
+    """Return function which calls merge_tracks for a random MIDI file."""
+    midi_files = load_midi_files(number)
+
+    def merge_tracks():
+        mid = midi_files[-1]
+        mido.merge_tracks(mid.tracks)
+        midi_files.pop()
+
+    return merge_tracks
+
+
+def custom_merge_tracks_fn(number: int) -> Callable:
+    """Return function which calls the custom implementation of merge_tracks for a random MIDI file."""
+    midi_files = load_midi_files(number)
+
+    def merge_tracks():
+        mid = midi_files[-1]
+        utils.merge_tracks(mid)
+        midi_files.pop()
+
+    return merge_tracks
+
+
+def bench(fn: Callable, number: int, repeat: int):
+    """Time the given function."""
+    times = timeit.repeat(fn, repeat=repeat, number=number)
+    print(f'Average time:       {np.mean(times) / number}')
+    print(f'Best average time:  {np.min(times) / number}')
+    print(f'Worst average time: {np.max(times) / number}')
+
+
+def bench_async(fn, number: int):
+    """Time the given async function."""
+    async def run():
+        await asyncio.gather(*[fn() for _ in range(number)])
+
+    start = time.perf_counter()
+    asyncio.run(run())
+    elapsed = time.perf_counter() - start
+
+    print(f'Total time:   {elapsed}')
+    print(f'Average time: {elapsed / number}')
+
+
+def profile(fn, number: int):
+    """Profile the given function."""
+    global_env = {'fn': fn, 'number': number}
+    cProfile.runctx(
+        '[fn() for _ in range(number)]',
+        globals=global_env,
+        locals=global_env,
+        sort=SortKey.CUMULATIVE
+    )
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 from typing import List
 
+import mido
+
 
 def get_midi_filepaths() -> List[str]:
     """Return list of filepaths of MIDI files."""
@@ -10,3 +12,31 @@ def get_midi_filepaths() -> List[str]:
         for filename in filenames
     ]
 
+
+def merge_tracks(mid: mido.MidiFile) -> mido.MidiTrack:
+    """
+    More efficient implementation of merge_tracks as compared to mido.
+    See mido.merge_tracks() for more information.
+    """
+    messages_abs_time = []
+    for track in mid.tracks:
+        elapsed = 0
+        for msg in track:
+            elapsed += msg.time
+            messages_abs_time.append((msg, elapsed))
+
+    messages_abs_time.sort(key=lambda msg: msg[1])
+
+    messages_rel_time = []
+    elapsed = 0
+    for msg, abs_time in messages_abs_time:
+        delta = abs_time - elapsed
+
+        # Calling __setattr__ on the copied instance is much faster than passing the attribute to copy.
+        msg = msg.copy()
+        msg.time = delta
+
+        messages_rel_time.append(msg)
+        elapsed = abs_time
+
+    return mido.MidiTrack(mido.midifiles.tracks.fix_end_of_track(messages_rel_time))
